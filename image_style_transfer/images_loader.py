54d26fee8eb0b29dfa2545f166a66bd5bea990d7
Zach Moshe <zachmoshe@gmail.com>
2017-10-13 16:26:41 +0300

    smarter resize (maintain aspect ratio)

--- image_style_transfer/images_loader.py
+++ image_style_transfer/images_loader.py
@@ -33,19 +33,19 @@ class ImagesLoader():
                         output_dir=DEFAULT_OUTPUT_DIR, output_shape=DEFAULT_OUTPUT_SHAPE):
         if data_dir[-1] == '/':
             data_dir = data_dir[:-1]
-        self.styles_dir = data_dir + "/" + styles_dir
-        self.content_dir = data_dir + "/" + content_dir
-        self.output_dir = data_dir + "/" + output_dir
+        self.styles_dir = data_dir + styles_dir
+        self.content_dir = data_dir + content_dir
+        self.output_dir = data_dir + output_dir
         self.output_shape = output_shape
 
     def image(self, dir, n):
-        return _get_image_np(dir, n, self.output_shape)
+        return _get_image_np(dir, n, _get_resized_shape(self.output_shape))
     def style(self, n):
-        return _get_image_np(self.styles_dir, n, self.output_shape)
+        return _get_image_np(self.styles_dir, n, _get_resized_shape(self.output_shape))
     def content(self, n):
-        return _get_image_np(self.content_dir, n, self.output_shape)
+        return _get_image_np(self.content_dir, n, _get_resized_shape(self.output_shape))
     def output(self, n):
-        return _get_image_np(self.outputs_dir, n, self.output_shape)
+        return _get_image_np(self.outputs_dir, n, _get_resized_shape(self.output_shape))
 
     def content_and_style(self, content, style):
         content_img = _get_image(self.content_dir, content)
